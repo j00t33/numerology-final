548fccb02a0cc09c3f93996b27efce5a37832f1e
j00t33 <jyoti.dexter2008@gmail.com>
2015-09-18 01:41:37 +0530

    First commit

---
+++ app/controllers/people_controller.rb
@@ -0,0 +1,65 @@
+get '/people' do
+	@people = Person.all
+	erb :"/people/index"
+end
+
+get '/people/new' do
+	@person = Person.new
+	erb :"/people/new"
+end
+
+get '/people/:id/edit' do
+	@person = Person.find(params[:id])
+	erb :"/people/edit"
+end
+
+post '/people' do
+	if params[:birthdate].include?("-")
+    birthdate = params[:birthdate]
+  else
+    birthdate = Date.strptime(params[:birthdate], "%m%d%Y")
+  end
+
+	@person = Person.new(first_name: params[:first_name], last_name: params[:last_name], birthdate: birthdate)
+  if @person.valid?
+    @person.save
+    redirect "/people/#{@person.id}"
+  else
+    @person.errors.full_messages.each do |message|
+      @errors = "#{@errors} #{message}."
+    end
+    erb :"/people/new"
+  end
+	
+end
+
+put '/people/:id' do
+	@person = Person.find(params[:id])
+  @person.first_name = params[:first_name]
+  @person.last_name = params[:last_name]
+  @person.birthdate = params[:birthdate]
+  if @person.valid?
+    @person.save
+    redirect "/people/#{@person.id}"
+  else
+    @person.errors.full_messages.each do |message|
+      @errors = "#{@errors} #{message}."
+    end
+    erb :"/people/edit"    
+  end   
+end
+
+delete '/people/:id' do
+	person = Person.find(params[:id])
+	person.destroy
+	redirect "/people"
+end
+
+get '/people/:id' do
+	@person = Person.find(params[:id])
+	birthdate_string = @person.birthdate.strftime("%m%d%Y")
+    birth_path_num = Person.get_birth_path_num(birthdate_string)
+    @message = Person.get_message(birth_path_num)
+	erb :"/people/show"
+end
+
